7991bb28cff759e64a34715145eaf540e8bf537d
Andrea <osoriop.0129@gmail.com>
2022-12-12 18:24:54 -0500

    Inicios del código de js

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-function sumatoria() {
-    let resultado = parseInt(numero1) + parseInt(numero2);
-    console.log(resultado);
-  }
+let num1=0;
+let ope;
+
+function f1(valor){
+  num1= num1 + valor
+  document.getElementById("resultadonum").innerHTML = num1;
+}
+
+function operacion(valor) {
+  ope = valor
   
-  function resta(atributoA, atributoB) {
-    let resultado2 = parseInt(atributoA) - parseInt(atributoB);
-    console.log(resultado2);
   }
-  
-  function product() {
-    let resultado3 = parseInt(numero1) * parseInt(numero2);
-    return resultado3;
-  }
-  
-  function division(atributoAa, atributoBb) {
-    let resultado4 = parseInt(atributoAa) / parseInt(atributoBb);
-    return resultado4;
-  }
-  
-  if (Operacion === "+") {
-    sumatoria();
-  } else if (Operacion === "-") {
-    resta(numero1, numero2);
-  } else if (Operacion === "*") {
-    console.log(product());
-  } else if (Operacion === "/") {
-    console.log(division(numero1, numero2));
-  } else {
-    console.log("No reconozco esa operacion");
-  }
-  
+}
